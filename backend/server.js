d2df956a4c5c38cb1b3b3b009a70d2b3708004a3
Junior Oyewunmi <junioroyewunmi@Juniors-MacBook-Air.local>
2022-11-27 13:51:48 +0100

    adding more functionality to the admin routes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
 // import stripeRouter from "./routers/stripeRouters.js";
+import uploadRouter from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.get("/api/keys/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
+app.use("/api/upload", uploadRouter);
 // app.use("/api/stripe", stripeRouter);
 app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
